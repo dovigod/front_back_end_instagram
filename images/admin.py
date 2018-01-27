376b5f89b22976aa2a62c9e649711236c6b28d64
dovigod <inaki96@naver.com>
2018-01-27 21:03:09 +0900

    fixed admin
    daw

--- images/admin.py
+++ images/admin.py
@@ -2,5 +2,37 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Image)
+
+class Image__admin(admin.ModelAdmin):
+
+    ordering = (
+        'caption',
+        'created_by',
+    )
+
+    list_display = (
+        'location',
+        'caption',
+        'created_by',
+        'updated_at',
+        'created_at',
+    )
+
+    list_display_links = (
+        'caption',
+        'location',
+    )
+
+    search_fields = (
+        'location',
+    )
+
+    list_filter = (
+        'created_at',
+        'location',
+    )
+
+
+admin.site.register(models.Image, Image__admin)
 admin.site.register(models.Comment)
+admin.site.register(models.Like)
--- images/models.py
+++ images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -9,8 +9,26 @@ class Image(models.Model):
     caption = models.TextField(max_length=10)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.location}-{self.caption}'
 
 
 class Comment(models.Model):
     content = models.TextField()
+    created_for = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     written_at = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.written_at}'
+
+
+class Like(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_at = models.TimeField(auto_now_add=True)
+    created_for = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.created_for}-{self.created_by}-{self.created_at}'
